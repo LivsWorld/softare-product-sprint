fbf7b44617eb3964322025b3c0f8785a99860a99
Olivia <shen.olivia.us@gmail.com>
2022-06-13 01:50:18 +0000

    Modify servlet to send list of messages

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,6 +1,10 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import com.google.gson.Gson;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,10 +13,24 @@ import javax.servlet.http.HttpServletResponse;
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
+  /** List of random messages to send */
+  private ArrayList<String> messageList = new ArrayList<>(
+    Arrays.asList(
+                  "Here's a fun fact about me",
+                  "This is another fun fact!",
+                  "Yet another interesting fact"
+    ));
 
+  /** When /hello is GET requested, returns messageList to client as JSON string */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Olivia!</h1>");
+    response.setContentType("application/json;");
+    response.getWriter().println(convertToJson(messageList));
+  }
+
+  /** Converts ArrayList of messages into JSON string with Gson library */
+  private String convertToJson(ArrayList<String> messages) {
+    Gson gson = new Gson();
+    return gson.toJson(messages);
   }
 }
